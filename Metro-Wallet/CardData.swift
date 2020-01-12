d179b223ff0855c2c6d163a1b400f3e7d886eebc
Matthew Ramos <mmramos309@gmail.com>
2020-01-11 21:47:43 -0500

    Combined classes of limited and unlimited card data into one, uses optionals to represent data that is not a property of all types. Renamed class accordingly. Removed extraneous code.

--- Metro-Wallet/CardData.swift
+++ Metro-Wallet/CardData.swift
@@ -8,12 +8,6 @@
 
 import Foundation
 
-protocol CardData {
-    var name: String { get set }
-    var expiration: Date { get set } //check if get or get set is more applicable
-    var cardType: CardType { get set }
-}
-
 enum CardType {
     case regular
     case reducedFare
@@ -22,36 +16,32 @@ enum CardType {
     case thirtyDay
 }
 
-class BalanceCardData: CardData {
+class MetroCard {
     var name: String
     var cardType: CardType
-    var balance: Double
+    var balance: Double?
     var expiration: Date
-    var decrementValue: Double
+    var decrementValue: Double?
+    var unlimitedExpiration: Date?
+    var numberOfRides: Int?
     
-    init(name: String, cardType: CardType, balance: Double, expiration: Date, decrementValue: Double) {
+    init(name: String, cardType: CardType, balance: Double?, expiration: Date, decrementValue: Double?, unlimitedExpiration: Date?, numberOfRides: Int?) {
         self.name = name
         self.cardType = cardType
         self.balance = balance
         self.expiration = expiration
         self.decrementValue = decrementValue
+        self.unlimitedExpiration = unlimitedExpiration
+        self.numberOfRides = numberOfRides
     }
     
     static let balanceCards = [
-        BalanceCardData(name: "Regular", cardType: CardType.regular, balance: 0 , expiration: Date(), decrementValue: 2.75),
-        BalanceCardData(name: "ReducedFare", cardType: CardType.reducedFare, balance: 0, expiration: Date(), decrementValue: 1.35)
+        MetroCard(name: "Regular", cardType: CardType.regular, balance: 0 , expiration: Date(), decrementValue: 2.75, unlimitedExpiration: nil, numberOfRides: nil),
+        MetroCard(name: "ReducedFare", cardType: CardType.reducedFare, balance: 0, expiration: Date(), decrementValue: 1.35, unlimitedExpiration: nil, numberOfRides: nil),
+        MetroCard(name: "Student", cardType: CardType.student, balance: nil, expiration: Date(), decrementValue: nil, unlimitedExpiration: nil, numberOfRides: 3),
+        MetroCard(name: "SevenDay", cardType: CardType.sevenDay, balance: nil, expiration: Date(), decrementValue: nil, unlimitedExpiration: addSevenDays(), numberOfRides: nil),
+        MetroCard(name: "ThirtyDay", cardType: CardType.thirtyDay, balance: nil, expiration: Date(), decrementValue: nil, unlimitedExpiration: addThirtyDays(), numberOfRides: nil)
     ]
-    
-    static let allBalanceCards: [BalanceCardData] = [BalanceCardData(name: "Regular", cardType: CardType.regular, balance: 0, expiration: Date(), decrementValue: 2.75), BalanceCardData(name: "ReducedFare", cardType: CardType.reducedFare, balance: 0, expiration: Date(), decrementValue: 0), BalanceCardData(name: "Student", cardType: CardType.student, balance: 0, expiration: Date(), decrementValue: 0)]
-    
-}
-
-class UnlimitedCardData: CardData {
-    var name: String
-    var expiration: Date
-    var unlimitedExpiration: Date
-    var cardType: CardType
-    var numberOfRides: Double
     
     static func addThirtyDays() -> Date? {
             var timeValue = DateComponents()
@@ -67,19 +57,4 @@ class UnlimitedCardData: CardData {
             return sevenDayExpirationDate
         }
     
-    init(name: String, cardType: CardType, expiration: Date, unlimitedExpiration: Date, numberOfRides: Double) {
-        self.name = name
-        self.cardType = cardType
-        self.unlimitedExpiration = unlimitedExpiration
-        self.expiration = expiration
-        self.numberOfRides = numberOfRides
-    }
-    
-    static let unlimitedCards = [
-        UnlimitedCardData(name: String(), cardType: CardType.student, expiration: Date(), unlimitedExpiration: Date(), numberOfRides: 3),
-        UnlimitedCardData(name: String(), cardType: CardType.sevenDay, expiration: Date(), unlimitedExpiration: Date(), numberOfRides: Double.infinity),
-        UnlimitedCardData(name: String(), cardType: CardType.thirtyDay, expiration: Date(), unlimitedExpiration: Date(), numberOfRides: Double.infinity)
-    ]
-    
-    static let allUnlimitedCards: [UnlimitedCardData] = [UnlimitedCardData(name: "Student", cardType: CardType.student, expiration: Date(), unlimitedExpiration: Date(), numberOfRides: 3), UnlimitedCardData(name: "SevenDay", cardType: CardType.sevenDay, expiration: Date(), unlimitedExpiration: addSevenDays() ?? Date(), numberOfRides: Double.infinity), UnlimitedCardData(name: "ThirtyDay", cardType: CardType.thirtyDay, expiration: Date(), unlimitedExpiration: addThirtyDays() ?? Date(), numberOfRides: Double.infinity)]
 }
